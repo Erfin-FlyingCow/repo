3e3a2c7bef58c9009265914eb4cfc5e07d8a9d42
Muhammad Irfan Noufal <erfintheflyngcow@gmail.com>
2023-06-29 12:58:20 +0700

    first commit

---
+++ tugasbesar2/src/Test.js
@@ -0,0 +1,11 @@
+function Test () {
+    return (
+        <div className="Test">
+              <h1 className="text-5xl font-bold text-center">
+                Hello world!
+              </h1>
+              </div>
+            );
+          }
+
+export default Test;
